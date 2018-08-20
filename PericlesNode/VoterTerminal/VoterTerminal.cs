a0adc014729fbcc62e40e7b45f67fd2fbc1a5528
asalkanovic <zekija816@gmail.com>
2018-08-20 15:14:17 -0500

    Updated VoterTerminal class file with FPTP user terminal method component completed.

--- PericlesNode/VoterTerminal/VoterTerminal.cs
+++ PericlesNode/VoterTerminal/VoterTerminal.cs
@@ -7,6 +7,7 @@ using Pericles.Crypto;
 using VoterDatabase;
 using ElectionModels;
 using ElectionModels.FirstPastThePost;
+using ElectionModels.InstantRunoff;
 using Pericles.Votes;
 
 namespace Pericles.VoterTerminal
@@ -69,8 +70,8 @@ namespace Pericles.VoterTerminal
                     Console.WriteLine("You've exhausted your tries. Bye Bye");
                     return;
                 }
-                FirstPastThePostVote fptpVote = new FirstPastThePostVote(candidateArr[choice + 1]);
-                var jsonVote = this.voteSerializer.Serialize(fptpVote);
+                FirstPastThePostVote irVote = new FirstPastThePostVote(this.candidateArr[choice - 1]);
+                var jsonVote = this.voteSerializer.Serialize(irVote);
                 var signature = SignatureProvider.Sign(this.password, this.keyPair, jsonVote.GetBytes());
                 Vote vote = new Vote(this.keyPair.PublicKey.GetBase64String(), jsonVote, signature.GetBase64String());
             }
@@ -80,8 +81,35 @@ namespace Pericles.VoterTerminal
                 Console.WriteLine("Welcome to the election! These are the following candidates to select from:\n" +
                     string.Join("\n", this.candidateArr.Select(x => $"{i++}: {x}"))
                     );
-                Console.WriteLine("Please enter an ordered list delimited by a comma e.g. 1, 3, 2.");
                 // We need to consider when a candidate is not wishing NONE
+                int counter = 1;
+                var prefs = "";
+                char[] charSeparators = new char[] { ',' };
+                while (counter < 4)
+                {
+                    Console.WriteLine("Please type a list in order of candidate preference e.g. 2, 1, 3.");
+                    prefs= Console.ReadLine();
+                    string[] tokens = prefs.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+                    if () //TODO validation check of voter input check
+                    {
+                        Console.WriteLine("Please enter a valid choice.");
+                        counter += 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (counter == 4)
+                {
+                    Console.WriteLine("You've exhausted your tries. Bye Bye");
+                    return;
+                }
+                InstantRunoffVote iroVote = new InstantRunoffVote(); // make the IR vote
+                var jsonVote = this.voteSerializer.Serialize(iroVote);
+                var signature = SignatureProvider.Sign(this.password, this.keyPair, jsonVote.GetBytes());
+                Vote vote = new Vote(this.keyPair.PublicKey.GetBase64String(), jsonVote, signature.GetBase64String());
             }
         }
 
